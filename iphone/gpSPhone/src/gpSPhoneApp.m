00eec0a2ad1b07146bff5e2d56c4d46c004f38ab
zach drayer <zach@drayer.name>
2011-12-30 22:06:28 -0500

    proper variable name

--- iphone/gpSPhone/src/gpSPhoneApp.m
+++ iphone/gpSPhone/src/gpSPhoneApp.m
@@ -91,7 +91,7 @@ static void noteCurrentSystemVolume(void * inUserData, AudioSessionPropertyID in
 		[ noROMSheet addButtonWithTitle:@"OK" ];
 		[ noROMSheet setDelegate:self ];
 		[ noROMSheet showInView:mainView ];
-		[ noRomSheet release ];
+		[ noROMSheet release ];
 	}
 
 	/* Initialize stats bar icons and notification on first good run */
